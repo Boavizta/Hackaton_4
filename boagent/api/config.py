764f2cf6df267f6a48f2906b7d7279f37f578686
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-01-06 15:05:45 +0100

    docs: update tags for csv and process with description

--- boagent/api/config.py
+++ boagent/api/config.py
@@ -3,15 +3,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     project_name: str = "boagent"
-    project_version: str = "0.1.0"
+    project_version: str = "0.1.1"
     project_description: str = "Boagent is a local API and monitoring agent to help you estimate the environmental impact of your machine, including software activity and hardware embodied impacts."
     tags_metadata: list = [
         {"name": "info", "description": "Returns runtime configuration of Boagent."},
         {"name": "web", "description": "Web UI to explore Boagent metrics."},
-        {
-            "name": "csv",
-            "description": "Internal route. Generates and returns a CSV-formatted dataset with metrics needed by the webUI",
-        },
         {
             "name": "metrics",
             "description": "Returns metrics as a Prometheus HTTP exporter.",
@@ -19,6 +15,10 @@ class Settings(BaseSettings):
         {
             "name": "query",
             "description": "This is the main route. Returns metrics in JSON format.",
+        },
+        {
+            "name": "process",
+            "description": "This route allows retrieving impacts for a given process. Returns metrics in JSON format.",
         },
     ]
     seconds_in_one_year: int = 31536000
